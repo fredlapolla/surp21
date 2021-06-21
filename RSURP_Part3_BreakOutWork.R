99753d81580cf924b0b770eff47974b690402572
fredlapolla <fred.lapolla@med.nyu.edu>
2021-06-21 10:06:12 -0400

    Add files via upload

---
+++ RSURP_Part3_BreakOutWork.R
@@ -0,0 +1,31 @@
+#R SURP Week 3 Group Work
+
+#Gruop Work 1
+
+#Try using is.na and na.omit to remove from CADMIUM and LEAD respectively.
+
+
+
+## Group Work 2
+
+#Try using recode_factor to change the values of DX_DBTS to "diabetes with dx", "diabetes no dx" and "no diabetes" (it's ok to abbreviate).
+
+
+
+
+
+## Group Work 3
+
+#Try using Mutate to make a new variable of Total cholesterol divided by HDL. This is not a real clinical metric (as far as I know) but is intended to practice Mutate. 
+
+
+
+
+## Group Work 4
+
+#Use GREP to find only the members of K with a B. Try also using gsub to change B to BGroup. 
+
+
+
+
+
